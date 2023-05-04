17e96cdd72a6e62a1aad00412f176358d67e3406
GustavoCostaHenriques <119442854+GustavoCostaHenriques@users.noreply.github.com>
2023-05-04 19:15:14 +0100

    Add files via upload

---
+++ Gustavo/Exercício3/Exercício3.R
@@ -0,0 +1,20 @@
+#Libraries
+library(ggplot2)
+
+#Reading the txt file
+setwd("C:\\Users\\gusta\\OneDrive\\Ambiente de Trabalho\\IST\\2º Ano 2º Semestre\\PE\\Projeto Computacional\\Gustavo\\Exercício3")
+data <- read.delim("Exercício3.txt", header = TRUE, sep = "\t", quote = "", dec = ".", fill = TRUE, comment.char = "")
+
+# Filter for Italy and the age groups
+filtered_data <- data[data$Country == "Italy" & data$TIME == "2019" & data$Age.Group %in% c("15-24", "25-54", "55-64") & data$Sex %in% c("Men", "Women") & data$IND == "EMP2", ]
+
+# Creating the plot
+ggplot(filtered_data, aes(x = AGE, y = Value, fill = SEX)) + 
+  geom_col(position = "dodge") + 
+  scale_x_discrete(labels = c("15-24", "25-54", "55-64")) + 
+  scale_fill_manual(values = c("#377eb8", "#DE3163"), name = "Sex") +
+  labs(title = "Labour force participation rate, by sex and age group in Italy (2019)",
+       x = "Age Group",
+       y = "Labour force participation rate, by sex and age group") +
+  theme(plot.title = element_text(hjust = 0.5)) + theme_bw()
+
